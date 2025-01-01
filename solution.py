491d22f45327a659422a968f7b062dca8a810b1d
Kooreis <kooreis.official@gmail.com>
2025-01-01 08:51:43 -0600

    This update adds two functions: 'findDuplicateSubtrees' and 'collect'. The 'findDuplicateSubtrees' function initializes a dictionary to count the occurrences of each subtree and a list to store the duplicate subtrees. The 'collect' function serializes each subtree into a string and checks if it has been seen before. If a subtree has been seen twice, it is added to the answer list.

--- solution.py
+++ solution.py
@@ -1,5 +1,13 @@
-class Node:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+def findDuplicateSubtrees(root):
+    count = {}
+    ans = []
+    collect(root, count, ans)
+    return ans
+
+def collect(node, count, ans):
+    if not node: return "#"
+    serial = "{},{},{}".format(node.val, collect(node.left, count, ans), collect(node.right, count, ans))
+    count[serial] = count.get(serial, 0) + 1
+    if count[serial] == 2:
+        ans.append(node)
+    return serial
