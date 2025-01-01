e2d69726bf8a58565a9a309d6914bb262318e2d8
Kooreis <kooreis.official@gmail.com>
2025-01-01 08:51:43 -0600

    This part of the script defines the main class and the main method. It creates a binary tree and initializes the map and list that will be used to store the string representations of the subtrees and the duplicate nodes respectively. It then calls the inorder function to start the process of finding duplicate subtrees.

--- solution.java
+++ solution.java
@@ -1,10 +1,19 @@
-import java.util.*;
+class Main {
+    static Map<String, Integer> m;
+    static List<Node> dups;
 
-class Node {
-    int data;
-    Node left, right;
-    Node(int data) {
-        this.data = data;
-        left = right = null;
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.right = new Node(3);
+        root.right.left = new Node(2);
+        root.right.left.left = new Node(4);
+        root.right.right = new Node(4);
+        m = new HashMap<>();
+        dups = new ArrayList<>();
+        inorder(root);
+        for (Node node : dups) {
+            System.out.println(node.data);
+        }
     }
-}
